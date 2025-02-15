22a096dcdec929ce4dc303eb303a0aeee89ca0d0
hoailinh007 <nhatkhung1837@gmail.com>
2025-02-15 10:48:11 +0700

    Add pages
    Add consultant page

---
+++ client/src/pages/guest/Consultant.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+
+export default function Consultant() {
+  return (
+    <div className="main-container w-[1920px] h-[1481.333px] bg-[#f9faef] relative overflow-hidden mx-auto my-0">
+      <div className="w-[1920px] h-[149.333px] relative overflow-hidden z-[4] mt-0 mr-0 mb-0 ml-[-1.335px]">
+        <div className="w-[1920px] h-[156px] bg-[#c86c79] absolute top-0 left-0 z-[5]" />
+        <div className="w-[213.333px] h-[74.667px] bg-[url(../assets/images/aadf2e48c583e2b73633377d2cb3aeb039255f44.png)] bg-cover bg-no-repeat absolute top-[34.663px] left-[120.002px] z-[17]" />
+        <div className="flex w-[350.667px] h-[68px] pt-0 pr-[21.333px] pb-0 pl-[5.333px] gap-[368px] items-center flex-nowrap bg-[#f3edf7] rounded-[80px] absolute top-[38.662px] left-[1323.998px] overflow-hidden z-10">
+          <input className="w-[350.667px] h-[68px] shrink-0 bg-transparent border-none absolute top-0 left-0 z-[16]" />
+          <div className="flex w-[64px] gap-[10.667px] items-center shrink-0 flex-nowrap relative z-[11]">
+            <div className="flex w-[64px] h-[64px] flex-col gap-[13.333px] justify-center items-center shrink-0 flex-nowrap relative z-[12]">
+              <div className="flex w-[53.333px] gap-[13.333px] justify-center items-center shrink-0 flex-nowrap rounded-[133.333px] relative overflow-hidden z-[13]">
+                <div className="flex w-[53.333px] pt-[10.667px] pr-[10.667px] pb-[10.667px] pl-[10.667px] gap-[13.333px] justify-center items-center shrink-0 flex-nowrap relative z-[14]">
+                  <div className="w-[32px] h-[32px] shrink-0 bg-[url(../assets/images/4b9ee1bd-53f7-4cf3-a24f-7fdff566c6b5.png)] bg-[length:100%_100%] bg-no-repeat relative z-[15]" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[61.333px] h-[61.333px] bg-[url(../assets/images/bd596c68-7e5e-4180-a982-9a0f397ba7d9.png)] bg-cover bg-no-repeat absolute top-[41.332px] left-[1708.002px] z-[9]" />
+        <span className="flex w-[74px] h-[35px] justify-center items-center font-['Lato'] text-[26.66666603088379px] font-semibold leading-[34.667px] text-[#000] absolute top-[54.661px] right-[1176.668px] text-center whitespace-nowrap z-[6]">
+          About
+        </span>
+        <span className="flex w-[257px] h-[35px] justify-center items-center font-['Lato'] text-[26.66666603088379px] font-semibold leading-[34.667px] text-[#141e0c] absolute top-[54.661px] right-[820.333px] text-center whitespace-nowrap z-[7]">
+          Skincare Consultation
+        </span>
+        <span className="flex w-[53px] h-[35px] justify-center items-center font-['Lato'] text-[26.66666603088379px] font-semibold leading-[34.667px] text-[#000] absolute top-[54.661px] left-[1198.669px] text-center whitespace-nowrap z-[8]">
+          Blog
+        </span>
+      </div>
+      <div className="w-[1920px] h-[97.333px] bg-[#ffc0cb] relative z-[2] mt-0 mr-0 mb-0 ml-[-1.335px]">
+        <span className="flex w-[506.667px] h-[38.667px] justify-center items-start font-['Newsreader'] text-[32px] font-extralight leading-[32.01px] text-[#000] absolute top-[35.998px] left-[733.331px] text-center whitespace-nowrap z-[2]">
+          Choose your consultant
+        </span>
+        <span className="flex w-[102.667px] h-[34.667px] justify-center items-start font-['Lato'] text-[29.333332061767578px] font-medium leading-[34.667px] text-[#000] absolute top-[57.332px] left-[299.999px] text-center whitespace-nowrap">
+          Nigga 1
+        </span>
+      </div>
+      <div className="bg-[url(../assets/images/e2f4cb23d15d0b89c721b683e5988c0c85b40335.png)] bg-cover bg-no-repeat relative z-[3] mt-[10.668px] mr-0 mb-0 ml-[958.664px]" />
+      <div className="w-[1805.332px] h-[513.333px] relative z-[27] mt-[133.332px] mr-0 mb-0 ml-[57.331px]">
+        <div className="w-[465.333px] h-[513.333px] bg-[url(../assets/images/07ff4aebfa5130c987eaa466c5493d182f7b52cb.png)] bg-cover bg-no-repeat absolute top-0 left-0 z-[23]" />
+        <div className="w-[513.333px] h-[513.333px] bg-[url(../assets/images/a15c83e15f73f9264c1f39566adb62de7681b1cf.png)] bg-cover bg-no-repeat absolute top-0 left-[629.341px] z-[26]" />
+        <div className="w-[513.333px] h-[513.333px] bg-[url(../assets/images/f7870737069e215bcf7eec9a1280ce87196eb1e4.png)] bg-cover bg-no-repeat absolute top-0 left-[1291.998px] z-[27]" />
+      </div>
+      <div className="w-[1374.668px] h-[49.334px] relative z-30 mt-[21.332px] mr-0 mb-0 ml-[254.663px]">
+        <span className="flex w-[72px] h-[48px] justify-center items-start font-['Newsreader'] text-[40px] font-normal leading-[48px] text-[#000] tracking-[-0.8px] absolute top-0 left-0 text-center whitespace-nowrap z-[28]">
+          Josh
+        </span>
+        <span className="flex w-[87px] h-[48px] justify-center items-start font-['Newsreader'] text-[40px] font-normal leading-[48px] text-[#000] tracking-[-0.8px] absolute top-[1.334px] left-[645.335px] text-center whitespace-nowrap z-[24]">
+          Mike
+        </span>
+        <span className="flex w-[48px] h-[48px] justify-center items-start font-['Newsreader'] text-[40px] font-normal leading-[48px] text-[#000] tracking-[-0.8px] absolute top-[1.334px] left-[1326.668px] text-center whitespace-nowrap z-30">
+          Ali
+        </span>
+      </div>
+      <div className="flex w-[1750.668px] h-[74.67px] justify-between items-center relative z-[31] mt-[10.667px] mr-0 mb-0 ml-[71.996px]">
+        <span className="flex w-[434.667px] h-[72px] justify-center items-start shrink-0 font-['Newsreader'] text-[20px] font-normal leading-[24px] text-[#000] tracking-[-0.4px] relative text-center z-[29]">
+          Reviva’s director of education offers decades of experience and vast
+          array of skincare knowledge & experience
+        </span>
+        <span className="flex w-[434.667px] h-[72px] justify-center items-start shrink-0 font-['Newsreader'] text-[20px] font-normal leading-[24px] text-[#000] tracking-[-0.4px] relative text-center z-[25]">
+          Reviva’s director of education offers decades of experience and vast
+          array of skincare knowledge & experience
+        </span>
+        <span className="flex w-[434.667px] h-[72px] justify-center items-start shrink-0 font-['Newsreader'] text-[20px] font-normal leading-[24px] text-[#000] tracking-[-0.4px] relative text-center z-[31]">
+          Reviva’s director of education offers decades of experience and vast
+          array of skincare knowledge & experience
+        </span>
+      </div>
+      <div className="flex w-[358.304px] h-[35px] justify-between items-center relative z-[39] mt-[179.999px] mr-0 mb-0 ml-[786.664px]">
+        <div className="w-[34.667px] h-[34.667px] shrink-0 bg-[url(../assets/images/69e35be1-53fe-44f1-b325-84f5f66cfa8a.png)] bg-cover bg-no-repeat relative z-[32]" />
+        <span className="flex w-[17.333px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-black leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[34]">
+          1
+        </span>
+        <span className="flex w-[17.333px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-light leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[38]">
+          2
+        </span>
+        <span className="flex w-[17.333px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-light leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[37]">
+          3
+        </span>
+        <span className="flex w-[17.333px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-light leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[36]">
+          4
+        </span>
+        <span className="flex w-[17.333px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-light leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[35]">
+          5
+        </span>
+        <span className="flex w-[28px] h-[35px] justify-center items-start shrink-0 font-['Lato'] text-[29.333332061767578px] font-light leading-[35px] text-[#000] relative text-center whitespace-nowrap z-[39]">
+          ....
+        </span>
+        <div className="w-[40.97px] h-[34.667px] shrink-0 bg-[url(../assets/images/08f9ecba-9a65-495e-abfe-c8a7a877aa81.png)] bg-cover bg-no-repeat relative z-[33]" />
+      </div>
+      <button className="w-[268px] h-[65.333px] bg-[#f7f5f4] rounded-[162.667px] border-solid border-[1.333px] border-[#000] relative z-[19] pointer mt-[31.663px] mr-0 mb-0 ml-[830.665px]">
+        <div className="w-[268px] h-[65.333px] bg-[#f7f5f4] rounded-[162.667px] border-solid border-[1.333px] border-[#000] absolute top-[-1.333px] left-[-1.333px] z-[21]">
+          <span className="flex w-[72px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] absolute top-[20px] left-[94.666px] text-center whitespace-nowrap z-20">
+            Confirm
+          </span>
+          <span className="flex w-[72px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] absolute top-[20px] left-[94.666px] text-center whitespace-nowrap z-[22]">
+            Confirm
+          </span>
+        </div>
+      </button>
+      <span className="flex w-[56px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] relative text-center whitespace-nowrap z-[18] mt-[12.003px] mr-0 mb-0 ml-[934.667px]">
+        cancel
+      </span>
+      <div className="w-[1548.004px] h-[89.33px] absolute top-[1066.665px] left-[174.664px] z-[68]">
+        <div className="flex w-[229.333px] h-[34.199px] gap-[12.07px] items-start flex-nowrap absolute top-0 left-[1318.671px] z-[68]">
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[69]">
+            <div className="w-full h-full bg-[url(../assets/images/568c071e-bc29-46b2-a4f4-8ed8bdbc506f.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[70]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[71]">
+            <div className="w-[36.211px] h-[34.199px] bg-[url(../assets/images/8c6d1155-a8aa-497c-b21f-afb541c33461.png)] bg-[length:100%_100%] bg-no-repeat relative z-[72] mt-0 mr-0 mb-0 ml-0" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[73]">
+            <div className="w-full h-full bg-[url(../assets/images/a726824f-790d-48d6-8ef5-55777a7748b5.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[74]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[75]">
+            <div className="w-[36.211px] h-[34.199px] bg-[url(../assets/images/89ef4a59-47f4-44cf-bdd8-058806f99a74.png)] bg-[length:100%_100%] bg-no-repeat relative z-[76] mt-0 mr-0 mb-0 ml-0" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[77]">
+            <div className="w-full h-full bg-[url(../assets/images/45dd10ee-2324-4dda-a26f-551b49c97810.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[78]" />
+          </div>
+        </div>
+        <div className="flex w-[229.333px] h-[34.199px] gap-[12.07px] items-start flex-nowrap absolute top-[10.667px] left-[673.336px] z-[57]">
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[58]">
+            <div className="w-full h-full bg-[url(../assets/images/c637707e-e37c-4782-82d4-c91f8589cc7d.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[59]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[60]">
+            <div className="w-full h-full bg-[url(../assets/images/ac985319-8dcc-4c5b-ac2c-9266bb1a3433.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[61]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[62]">
+            <div className="w-full h-full bg-[url(../assets/images/7a274d32-46af-4f2a-bd99-bc951f678525.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[63]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[64]">
+            <div className="w-full h-full bg-[url(../assets/images/bc983be1-c91a-4115-b7ca-2ca399adb87c.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[65]" />
+          </div>
+          <div className="w-[36.211px] h-[34.199px] shrink-0 relative z-[66]">
+            <div className="w-full h-full bg-[url(../assets/images/b1c23424-a21b-4250-b9d3-b1600f2bfd81.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[67]" />
+          </div>
+        </div>
+        <div className="flex w-[229.333px] h-[50.768px] gap-[11.489px] items-start flex-nowrap absolute top-[13.33px] left-0 z-[46]">
+          <div className="w-[34.466px] h-[32.551px] shrink-0 relative z-[47]">
+            <div className="w-full h-full bg-[url(../assets/images/ccbe24ed-dfff-43ed-9cc3-6a628d587bd3.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[48]" />
+          </div>
+          <div className="w-[34.466px] h-[32.551px] shrink-0 relative z-[49]">
+            <div className="w-full h-full bg-[url(../assets/images/90e942f7-f974-4197-aa17-156ab9e0cb48.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-50" />
+          </div>
+          <div className="w-[34.466px] h-[32.551px] shrink-0 relative z-[51]">
+            <div className="w-full h-full bg-[url(../assets/images/a8c53ade-24c5-49f2-bac8-6ad0dd14f975.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[52]" />
+          </div>
+          <div className="w-[34.466px] h-[32.551px] shrink-0 relative z-[53]">
+            <div className="w-full h-full bg-[url(../assets/images/daf6d716-88aa-4db4-8008-b3b6f0412d17.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[54]" />
+          </div>
+          <div className="w-[34.466px] h-[32.551px] shrink-0 relative z-[55]">
+            <div className="w-full h-full bg-[url(../assets/images/ff7c2c0f-dc3a-48bf-aafb-531541dd75f2.png)] bg-[length:100%_100%] bg-no-repeat absolute top-0 left-0 z-[56]" />
+          </div>
+        </div>
+        <button className="w-[169.333px] h-[44px] bg-[#f7f5f4] rounded-[162.667px] border-solid border-[1.333px] border-[#000] absolute top-[45.33px] left-[1346.666px] z-[44] pointer">
+          <span className="flex w-[67px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] absolute top-[9.333px] left-[49.336px] text-center whitespace-nowrap z-[45]">
+            Choose
+          </span>
+        </button>
+      </div>
+      <div className="flex w-[836.001px] h-[48.005px] justify-between items-center absolute top-[1130.658px] left-[204px] z-[42]">
+        <button className="w-[169.333px] h-[44px] shrink-0 bg-[#f7f5f4] rounded-[162.667px] border-solid border-[1.333px] border-[#000] relative z-40 pointer">
+          <span className="flex w-[67px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] absolute top-[6.67px] left-[49.33px] text-center whitespace-nowrap z-[41]">
+            Choose
+          </span>
+        </button>
+        <button className="w-[169.333px] h-[44px] shrink-0 bg-[#f7f5f4] rounded-[162.667px] border-solid border-[1.333px] border-[#000] relative z-[42] pointer">
+          <span className="flex w-[67px] h-[24px] justify-center items-start font-['Lato'] text-[20px] font-normal leading-[24px] text-[#000] absolute top-[5.341px] left-[50.665px] text-center whitespace-nowrap z-[43]">
+            Choose
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
