e5941bebb1b652a1ebf45b0ee6656d73e1328d9b
WuKai12 <wusxocompany@gmail.com>
2025-02-16 16:49:01 +0700

    back to local

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://velvety-426i.vercel.app',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }, // Proxy all requests starting with "/api" to the backend
