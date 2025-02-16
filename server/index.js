84372cb9f4be1f81355daf322a9c89e6b3cf35ca
WuKai12 <wusxocompany@gmail.com>
2025-02-16 19:41:32 +0700

    Fix Vercel 404 issue

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend's URL
+  origin: 'https://velvety-five.vercel.app', // Replace with your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
