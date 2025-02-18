2a91f29ce93024f4e237d4a4a4797c5d5e3d1658
WuKai12 <wusxocompany@gmail.com>
2025-02-18 10:45:46 +0700

    fix vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: process.env.VITE_API_URL,
+    },
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL,
