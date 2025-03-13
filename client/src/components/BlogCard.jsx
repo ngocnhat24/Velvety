3e07be568e18a5ab14520ac2d5077e1326a9ff07
hoailinh007 <nhatkhung1837@gmail.com>
2025-03-13 15:44:37 +0700

    Update BlogCard.jsx
    update BlogCard

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -7,12 +7,6 @@ function BlogCard({ image, title, description, onReadMore }) {
       <div className="p-4 flex flex-col justify-between flex-grow">
         <h3 className="text-xl font-semibold text-[#AB152A]">{title}</h3>
         <p className="text-gray-600 text-sm mt-2">{description}</p>
-        <button
-          onClick={onReadMore}
-          className="mt-4 text-[#C86C79] hover:text-[#ffc0cb] self-end"
-        >
-          Read More
-        </button>
       </div>
     </div>
   );
